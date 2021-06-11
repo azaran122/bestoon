533116a63b810b2ea231c497850e56cff01ef75d
marziedarvish <marziedarvish@yahoo.com>
2021-06-11 07:16:58 -0700

    models updated and gitignore update and some butifications

--- web/admin.py
+++ web/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Expense
+from .models import Income
 
 
 admin.site.register(Expense)
+admin.site.register(Income)
